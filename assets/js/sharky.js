2a2eb8e082f758f526a38aaa6d95fe65977fcfc3
Greg Wilken <gregwilken@CALLISTO.local>
2017-04-11 21:24:40 -0700

    filter user input, fine tune decision tree

--- assets/js/sharky.js
+++ assets/js/sharky.js
@@ -7,17 +7,34 @@ var User = function(name) {
 	this.name = name;
 	this.wins = 0;
 	this.losses = 0;
+
+	this.addWin = function() {
+		this.wins++;
+	}
+
+	this.addLoss = function() {
+		this.losses++;
+	}	
+
+	this.getWins = function() {
+		return this.wins;
+	}
+
+	this.getLosses = function() {
+		return this.losses;
+	}
+
 }
 
 
-var Game = function(name, wordToGuess) {
+var Game = function(user, wordToGuess) {
 
 	console.log(wordToGuess);
 
+	this.user = user;
 	this.word = wordToGuess;
 	this.hiddenWord = '_'.repeat(this.word.length).split('');	
 	this.guessesLeft = this.word.length;
-	this.user = new User(name);
 	this.active = true;
 	this.lettersPlayed = {
 		inWord: [],
@@ -28,16 +45,27 @@ var Game = function(name, wordToGuess) {
 	var scum = document.getElementById("audioScum");
 	var arr = document.getElementById("audioArr");	
 
+	var instructions = "out with the PIRATE PASSWORD you filthy bilge rat! or down to Davey Jones' locker with ye!";
+	
+
 	document.getElementById("word").innerHTML = this.hiddenWord.join('');
 
 
 	this.guess = function(guessedLetter) {
 
+		if (guessedLetter.charCodeAt(0) < 65 || guessedLetter.charCodeAt(0) > 90 || guessedLetter === 'SHIFT' || guessedLetter === 'META' || guessedLetter === 'CONTROL' || guessedLetter === 'ALT' || guessedLetter === 'ARROWUP' || guessedLetter === 'ARROWDOWN'  || guessedLetter === 'ARROWLEFT'  || guessedLetter === 'ARROWRIGHT' || guessedLetter === 'CAPSLOCK' || guessedLetter === 'BACKSPACE') {
+
+			this.message('try entering some REAL characters you worthless peice of shark bait!');
+		
+		} else
+
 		if ( this.lettersPlayed.notInWord.includes(guessedLetter) || this.lettersPlayed.inWord.includes(guessedLetter) ) {
 
-				this.message('ya already guessed that letter you scurvy wentch!');
+				this.message('ya ALREADY GUESSED that letter you scurvy wentch!');
 			
-			} else if( !this.word.includes(guessedLetter) ) {
+			} else 
+
+				if( !this.word.includes(guessedLetter) ) {
 
 					this.lettersPlayed.notInWord.push(guessedLetter);
 					
@@ -63,11 +91,17 @@ var Game = function(name, wordToGuess) {
 
 	this.fail = function() {
 
+		document.getElementById("guessed").innerHTML = this.lettersPlayed.notInWord.join('');
+
 		this.guessesLeft--;
 		
-		scum.play();
+		if (this.guessesLeft === 0) {
+			this.endOfGame(false);
+		}
 
-		console.log(this.lettersPlayed.notInWord);
+		this.message('try ANOTHER LETTER you snivilling deck swab!');
+
+		scum.play();
 
 		if(this.guessesLeft <= 0) this.endOfGame();
 
@@ -76,44 +110,71 @@ var Game = function(name, wordToGuess) {
 
 	this.success = function() {
 
+		this.message("ARR that's the spirit!");
+
 		document.getElementById("word").innerHTML = this.hiddenWord.join('');
 
 		arr.play();
 
-	}
-
-
-	this.endOfGame = function() {
-
-		console.log('game over');
-		this.active === false;
-
-		//game over html logic
+		if (this.hiddenWord.join().includes('_') === false) {
+			this.endOfGame(true);
+			console.log('win');
+		}
 
 	}
+
+
+	this.endOfGame = function(win) {
+
+		if(win) {
+			this.user.addWin();
+		}
+
+		if(!win) {
+			this.user.addLoss();
+		}
+
+		this.message('try again you worthless deck swab! enter a NEW PIRATE PASSWORD!');
+		
+		console.log('wins', this.user.getWins());
+
+		console.log('losses', this.user.getLosses());
+
+
+		newGame = new Game(user, pirateWords[Math.floor(Math.random() * pirateWords.length)]);
+
+	}
+
 
 	this.message = function(msg) {
-		console.log('played');
-		document.getElementById("message").innerHTML = msg.toUpperCase();
+
+		document.getElementById("message").innerHTML = msg;
+	
 	}
 
 }
 
 
-var newGame = new Game('Greg', pirateWords[Math.floor(Math.random() * pirateWords.length)]);
+var user = new User('Greg');
+
+var newGame = new Game(user, pirateWords[Math.floor(Math.random() * pirateWords.length)]);
+
 
 
 document.onkeyup = function(event) {
 
-	if(newGame.active) {
+	if(newGame) {
 		
-		var userInput = event.key;
+		var userInput = event.key.toUpperCase();
 
-		newGame.guess(userInput);
+			console.log(userInput.charCodeAt(0));
 
-	} else console.log('Out of guesses!');
+			newGame.guess(userInput);
 
-}
+		} //else console.log('invalid key');
+	} 
+
+
 
 
 
